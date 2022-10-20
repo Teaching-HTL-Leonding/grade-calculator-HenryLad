7ebd5928806617a7511ae27577d2e928d2519fd4
Henry Lad <113623013+HenryLad@users.noreply.github.com>
2022-10-20 12:01:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿Console.WriteLine("Did you have particepated in the first written exam");
+string firstexam = Console.ReadLine()!;
+double totalpoints = 0;
+double possiblepoints = 0;
+
+if (firstexam == "yes"){
+    Console.WriteLine("How many points did you get 0 - 20 ");
+    int firstpoints = int.Parse(Console.ReadLine()!);
+    totalpoints += firstpoints;
+    possiblepoints += 20;
+}
+else {
+    Console.WriteLine("It is a shame");
+}
+Console.WriteLine("Did you particepate in a sceond exam");
+string sceondexam = Console.ReadLine()!;
+
+if  ( sceondexam == "yes"){
+    Console.WriteLine("How many points did you get in your second exam 0 - 20");
+    int secondpoints = int.Parse(Console.ReadLine()!);
+    totalpoints += secondpoints;
+    possiblepoints += 20;
+}
+else{
+    Console.WriteLine("What a shame");
+    
+}
+Console.WriteLine("Did you have any homework");
+string Ishomework = (Console.ReadLine()!);
+
+if (Ishomework == "yes"){
+    Console.WriteLine("How many points did you get on your homework");
+    int homework = int.Parse(Console.ReadLine()!);
+    possiblepoints += 20;
+    totalpoints += homework;
+}
+else {
+    Console.WriteLine("What a shame");
+}
+Console.WriteLine("Did you take a oral exams");
+string oral = Console.ReadLine()!;
+if (oral == "yes"){
+    Console.WriteLine("How many points did you get on your oral exam");
+    int oralpoints = int.Parse(Console.ReadLine()!);
+    possiblepoints += 20;
+    totalpoints += oralpoints;
+}
+else {
+    Console.WriteLine("What a shame");
+}
+
+
+
+double grade = totalpoints/possiblepoints * 100;
+Console.WriteLine(grade);
+if ( grade > 89){
+    Console.WriteLine("You grade is 1");
+}
+else if (grade >= 76){
+    Console.WriteLine("Your grade is 2");
+}
+else if (grade >= 63){
+    Console.WriteLine("Your grade is 3");
+
+}
+else if (grade >= 50){
+    Console.WriteLine("Your grade is a 4");
+}
+else if (grade <= 49){
+    Console.WriteLine("Sry you didn´t pass");
+}
